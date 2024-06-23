aa180cb031e93be4b49e86be4f9de953a528fae2
korpi <os2777ko-s@student.lu.se>
2024-06-23 12:06:07 +0200

    updated page, new layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/main/resources/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+        'default': ['Raleway', '"Segoe UI', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    },
+    extend: {
+        colors: {
+            'oxford-blue': '#112132',
+            'indigo-dye': '#274268',
+            'imperial-red': '#F03A47',
+            'imperial-red-dark': '#AB0D17',
+            'pale-azure': '#97DFFC'
+        },
+    },
   },
   plugins: [],
 }
