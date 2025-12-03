5b81948d9fd3254af09ca5f846643aec21ab09e3
o-korpi <oscar@korpi.se>
2025-12-03 15:25:53 +0100

    update website

--- src/main/kotlin/se/korpi/plugins/Routing.kt
+++ src/main/kotlin/se/korpi/plugins/Routing.kt
@@ -19,81 +19,81 @@ fun Application.configureRouting() {
             call.respond(PebbleContent("templates/index.html", mapOf()))
         }
         
-        get("/projects") {
-            call.respond(PebbleContent("templates/projects.html", mapOf()))
-        }
-        
-        get("/projects/conveyor") {
-            val conveyorYamlBasic = """
-                project:
-                  name: myProject
-                  group: com.example
-                  version: 0.0.1
-
-                  kotlinVersion: 2.0.0
-                  jvmTarget: 1.8  # Valid values: 1.8, 9, 10, ..., 21. Default 1.8
-                  mainClass: "com.example.myProject.MainKt"
-                  versions:
-
-                  plugins:
-
-                  dependencies:
-
-                  testDependencies:
-            """.trimIndent()
-            
-            val conveyorYaml = """
-                project:
-                  name: myProject
-                  group: com.example
-                  version: 0.0.1
-                  
-                  versions:
-                    kotlin: 2.0.0
-                    ktor: 2.3.12
-                    kotest: 5.9.1
-
-                  kotlinVersion: "${'$'}kotlin"
-                  jvmTarget: 21  # Valid values: 1.8, 9, 10, ..., 21. Default 1.8
-                  mainClass: "com.example.myProject.MainKt"
-                  
-                  plugins:
-                    - "kotlinx-serialization:org.jetbrains.kotlin:kotlin-maven-serialization:${'$'}kotlin"
-                    
-                  dependencies:
-                    - "org.jetbrains.kotlinx:kotlinx-serialization-json:${'$'}kotlin"
-                    - "io.ktor:ktor-server-core:${'$'}ktor"
-                    - "io.ktor:ktor-server-netty:${'$'}ktor"
-                    - "ch.qos.logback:logback-classic:1.4.14"
-                    
-                  testDependencies:
-                    - "io.kotest:kotest-runner-junit5:${'$'}kotest"
-                    - "io.kotest:kotest-assertions-core:${'$'}kotest"
-                    
-            """.trimIndent()
-            
-            call.respond(PebbleContent("templates/projects/conveyor.html", mapOf("basic" to conveyorYamlBasic, "yaml" to conveyorYaml)))
-        }
-        
-        get("/projects/sequdiff") {
-            call.respond(PebbleContent("templates/projects/sequdiff.html", mapOf()))
-        }
-        
-        get("/projects/apifetcher") {
-            call.respond(PebbleContent("templates/projects/apifetcher.html", mapOf()))
-        }
-        
-        get("/projects/dbml2kt") {
-            call.respond(PebbleContent("templates/projects/dbml2kt.html", mapOf()))
-        }
-        
-        get("/projects/query4k") {
-            call.respond(PebbleContent("templates/projects/query4k.html", mapOf()))
-        }
-        
-        get("/projects/kotlin-baseuid") {
-            call.respond(PebbleContent("templates/projects/kotlin-baseuid.html", mapOf()))
-        }
+//        get("/projects") {
+//            call.respond(PebbleContent("templates/projects.html", mapOf()))
+//        }
+//        
+//        get("/projects/conveyor") {
+//            val conveyorYamlBasic = """
+//                project:
+//                  name: myProject
+//                  group: com.example
+//                  version: 0.0.1
+//
+//                  kotlinVersion: 2.0.0
+//                  jvmTarget: 1.8  # Valid values: 1.8, 9, 10, ..., 21. Default 1.8
+//                  mainClass: "com.example.myProject.MainKt"
+//                  versions:
+//
+//                  plugins:
+//
+//                  dependencies:
+//
+//                  testDependencies:
+//            """.trimIndent()
+//            
+//            val conveyorYaml = """
+//                project:
+//                  name: myProject
+//                  group: com.example
+//                  version: 0.0.1
+//                  
+//                  versions:
+//                    kotlin: 2.0.0
+//                    ktor: 2.3.12
+//                    kotest: 5.9.1
+//
+//                  kotlinVersion: "${'$'}kotlin"
+//                  jvmTarget: 21  # Valid values: 1.8, 9, 10, ..., 21. Default 1.8
+//                  mainClass: "com.example.myProject.MainKt"
+//                  
+//                  plugins:
+//                    - "kotlinx-serialization:org.jetbrains.kotlin:kotlin-maven-serialization:${'$'}kotlin"
+//                    
+//                  dependencies:
+//                    - "org.jetbrains.kotlinx:kotlinx-serialization-json:${'$'}kotlin"
+//                    - "io.ktor:ktor-server-core:${'$'}ktor"
+//                    - "io.ktor:ktor-server-netty:${'$'}ktor"
+//                    - "ch.qos.logback:logback-classic:1.4.14"
+//                    
+//                  testDependencies:
+//                    - "io.kotest:kotest-runner-junit5:${'$'}kotest"
+//                    - "io.kotest:kotest-assertions-core:${'$'}kotest"
+//                    
+//            """.trimIndent()
+//            
+//            call.respond(PebbleContent("templates/projects/conveyor.html", mapOf("basic" to conveyorYamlBasic, "yaml" to conveyorYaml)))
+//        }
+//        
+//        get("/projects/sequdiff") {
+//            call.respond(PebbleContent("templates/projects/sequdiff.html", mapOf()))
+//        }
+//        
+//        get("/projects/apifetcher") {
+//            call.respond(PebbleContent("templates/projects/apifetcher.html", mapOf()))
+//        }
+//        
+//        get("/projects/dbml2kt") {
+//            call.respond(PebbleContent("templates/projects/dbml2kt.html", mapOf()))
+//        }
+//        
+//        get("/projects/query4k") {
+//            call.respond(PebbleContent("templates/projects/query4k.html", mapOf()))
+//        }
+//        
+//        get("/projects/kotlin-baseuid") {
+//            call.respond(PebbleContent("templates/projects/kotlin-baseuid.html", mapOf()))
+//        }
         
     }
 }
